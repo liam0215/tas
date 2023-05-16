f212566d4af73644db769c7af8c46ff620568ebc
Liam Arzola <liam0215@gmail.com>
2023-05-16 19:45:35 +0000

    change max_rx_pkt_len to 9000

--- tas/fast/network.c
+++ tas/fast/network.c
@@ -52,7 +52,7 @@
 uint8_t net_port_id = 0;
 static struct rte_eth_conf port_conf = {
     .rxmode = {
-      .max_rx_pkt_len = 8196,
+      .max_rx_pkt_len = 9000,
       .mq_mode = ETH_MQ_RX_RSS,
       .offloads = (DEV_RX_OFFLOAD_JUMBO_FRAME | DEV_RX_OFFLOAD_SCATTER | DEV_RX_OFFLOAD_CHECKSUM),
       .split_hdr_size = 0
