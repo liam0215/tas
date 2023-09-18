0e1980b2b4c974109d7af44cdafcc5292351584c
Liam Arzola <larzola@swsnetlab06.mpi-sws.org>
2023-09-19 00:29:36 +0200

    replace remounting of hugepages with clearing of hugepage directory

--- experiments/components/tas.py
+++ experiments/components/tas.py
@@ -67,11 +67,7 @@ class TAS:
         remove_tas_socket_com = "find {} -name \"*flexnic_os*\" | xargs rm -r".format(self.tas_config.project_dir)
         pane.send_keys(remove_tas_socket_com)
         time.sleep(1)
-        cmd = "sudo umount /dev/hugepages"
-        time.sleep(1)
-        cmd = "sudo mount -t hugetlbfs nodev /dev/hugepages"
-        self.pane.send_keys(cmd)
-        time.sleep(1)
-        cmd = "echo 16384 | sudo tee /sys/devices/system/node/node*/hugepages/hugepages-2048kB/nr_hugepages"
-        self.pane.send_keys(cmd)
-        time.sleep(5)
+        remove_tas_socket_com = "find /dev/hugepages/ -name \"*tas*\" | xargs rm -r"
+        pane.send_keys(remove_tas_socket_com)
+        time.sleep(2)
+
