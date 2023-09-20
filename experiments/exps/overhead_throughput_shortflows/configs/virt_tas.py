52a04de89438f930239e19548615fd455a94c39c
Liam Arzola <larzola@swsnetlab06.mpi-sws.org>
2023-09-20 20:26:38 +0200

    add tunnel flag to virtuoso

--- experiments/exps/overhead_throughput_shortflows/configs/virt_tas.py
+++ experiments/exps/overhead_throughput_shortflows/configs/virt_tas.py
@@ -49,7 +49,9 @@ class Config:
                 ip=self.s_machine_config.ip,
                 cset="tas_server",
                 n_cores=10,
-                pci="86:00.0")
+                pci="86:00.0",
+                tunnel=True,
+                cores=tas_cset.cores)
         tas_config.args = tas_config.args  + " --vm-shm-len=4294967296"
         self.s_tas_configs.append(tas_config)
 
@@ -63,7 +65,7 @@ class Config:
                 tas_dir=self.defaults.default_vtas_dir_bare,
                 tas_dir_virt=self.defaults.default_vtas_dir_virt,
                 idx=0,
-                n_cores=5,
+                n_cores=11,
                 cset="vm0_server",
                 memory=5)
 
@@ -78,7 +80,7 @@ class Config:
 
         server0_config = ServerConfig(pane=self.defaults.s_server_pane,
                 idx=0, vmid=0,
-                port=1234, ncores=2, max_flows=4096, max_bytes=msize,
+                port=1234, ncores=5, max_flows=4096, max_bytes=msize,
                 bench_dir=self.defaults.default_vbenchmark_dir_virt,
                 tas_dir=self.defaults.default_vtas_dir_virt)
         self.server_configs.append(server0_config)
@@ -104,7 +106,9 @@ class Config:
                 ip=self.c_machine_config.ip,
                 pci="d8:00.0",
                 cset="tas_client",
-                n_cores=10)
+                n_cores=10,
+                tunnel=True,
+                cores=tas_cset.cores)
         tas_config.args = tas_config.args  + " --vm-shm-len=4294967296"
         self.c_tas_configs.append(tas_config)
 
@@ -118,7 +122,7 @@ class Config:
                 tas_dir=self.defaults.default_vtas_dir_bare,
                 tas_dir_virt=self.defaults.default_vtas_dir_virt,
                 idx=0,
-                n_cores=5,
+                n_cores=11,
                 cset="vm0_client",
                 memory=5)
 
@@ -134,7 +138,7 @@ class Config:
         client0_config = ClientConfig(exp_name=exp_name, 
                 pane=self.defaults.c_client_pane,
                 idx=0, vmid=0, stack=self.cstack,
-                ip=self.s_vm_configs[0].vm_ip, port=1234, ncores=2,
+                ip=self.s_vm_configs[0].vm_ip, port=1234, ncores=5,
                 msize=msize, mpending=1, nconns=100,
                 open_delay=0, max_msgs_conn=flow_len, max_pend_conns=1,
                 bench_dir=self.defaults.default_vbenchmark_dir_virt,
